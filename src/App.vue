b191e12d618c992164f3a75e7c9d0a9ab18d7f70
wangyan <wangyan7@itcast.cn>
2020-07-06 16:28:52 +0800

    test3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo"
+         src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    hello docker
   </div>
 </template>
 
